5daebe15d08f4fcd02181d3882f6536849880caa
Kevenn <k.douglascaterinque@gmail.com>
2021-02-17 20:30:15 -0300

    more fixes

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,6 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap');
 
 body, select {
